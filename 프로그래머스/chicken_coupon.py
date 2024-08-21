3b399619f8ccd5708ffc3ce8d83ffdaa56e44844
ryojae <150101557+ryojae@users.noreply.github.com>
2024-08-21 09:43:09 +0900

    Delete 프로그래머스/chicken_coupon.py

--- 프로그래머스/chicken_coupon.py
+++
@@ -1,7 +0,0 @@
-def solution(chicken):
-    answer = 0
-    chicken_coupon = chicken
-    while chicken_coupon >= 10:
-        answer += chicken_coupon // 10
-        chicken_coupon = chicken_coupon % 10 + chicken_coupon // 10
-    return answer
